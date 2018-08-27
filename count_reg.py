53247bf873f24aa5a40bb9dce2b37fe15b6e8db3
Dominik Pataky <git@bitkeks.eu>
2018-08-27 20:50:37 +0200

    Some simplifications

--- count_reg.py
+++ count_reg.py
@@ -1,18 +1,15 @@
 import datetime
 # Script for counting money for ascii Dresden.
-# This is definitely no well written python. One of my first little projects. 
+# This is definitely no well written python. One of my first little projects.
 
 name = "Julius Felchow"
 
-"""
-Array that contains the values of bills and coins.
-"""
+# Array that contains the values of bills and coins.
 staggering = [100, 50, 20, 10, 5, 2, 1, .5, .2, .1, .05, .02, .01]
-"""
-Arrays for amounts of the bills and coins.
-"""
-amounts = [0 for i in range(13)]
-proposed_amounts = [0 for i in range(13)]
+
+# Arrays for amounts of the bills and coins.
+amounts = {coin: 0 for coin in staggering}
+proposed_amounts = {coin: 0 for coin in staggering}
 
 """
 Integers for money amount.
@@ -25,109 +22,63 @@ total = 0
 rest = 0
 register_money = 0
 
-def dev_values(amounts) :
-    """Sets arbitrary values for amounts for testing."""
-    # values for development
-    amounts[0] = 1
-    amounts[1] = 2
-    amounts[2] = 4
-    amounts[3] = 8
-    amounts[4] = 9
-    amounts[5] = 14
-    amounts[6] = 28
-    amounts[7] = 23
-    amounts[8] = 32
-    amounts[9] = 33
-    amounts[10] = 16
-    amounts[11] = 7
-    amounts[12] = 2
 
 def read_input(amounts) :
     """Reads the input for the amount of bills and coins in the register."""
-    try:
-        amounts[0] = int(input('Amount 100: '))
-        amounts[1] = int(input('Amount  50: '))
-        amounts[2] = int(input('Amount  20: '))
-        amounts[3] = int(input('Amount  10: '))
-        amounts[4] = int(input('Amount   5: '))
-        amounts[5] = int(input('Amount   2: '))
-        amounts[6] = int(input('Amount   1: '))
-        amounts[7] = int(input('Amount  .5: '))
-        amounts[8] = int(input('Amount  .2: '))
-        amounts[9] = int(input('Amount  .1: '))
-        amounts[10] = int(input('Amount .05: '))
-        amounts[11] = int(input('Amount .02: '))
-        amounts[12] = int(input('Amount .01: '))
-    except ValueError:
-        print("NaN")
+
+    for coin in staggering:
+        try:
+            amounts[coin] = int(input('Amount {}: '.format(coin)))
+        except ValueError:
+            print("NaN")
+
 
 def calculate_staggering():
     """
     Calculates a proposed staggering for the amount of bills and coins that should be put into the counter.
 
-    Definitely not fail proof and not intelligent yet. 
+    Definitely not fail proof and not intelligent yet.
     Adjust the staggering to keep in 5 and 10 Euro bills.
     """
-    proposed_amounts[10] = amounts[10]
-    proposed_amounts[11] = amounts[11]
-    proposed_amounts[12] = amounts[12]
-    for i in range(amounts[0]):
-        if get_overlap() >= staggering[0]:
-            proposed_amounts[0] += 1
-    for i in range(amounts[1]):
-        if get_overlap() >= staggering[1]:
-            proposed_amounts[1] += 1
-    for i in range(amounts[2]):
-        if get_overlap() >= staggering[2]:
-            proposed_amounts[2] += 1
-    for i in range(amounts[3]):
-        if get_overlap() >= staggering[3]:
-            proposed_amounts[3] += 1
-    for i in range(amounts[4]):
-        if get_overlap() >= staggering[4]:
-            proposed_amounts[4] += 1
-    for i in range(amounts[5]):
-        if get_overlap() >= staggering[5]:
-            proposed_amounts[5] += 1
-    for i in range(amounts[6]):
-        if get_overlap() >= staggering[6]:
-            proposed_amounts[6] += 1
-    for i in range(amounts[7]):
-        if get_overlap() >= staggering[7]:
-            proposed_amounts[7] += 1
-    for i in range(amounts[8]):
-        if get_overlap() >= staggering[8]:
-            proposed_amounts[8] += 1
-    for i in range(amounts[9]):
-        if get_overlap() >= staggering[9]:
-            proposed_amounts[9] += 1
+
+    # Coins which are always put away
+    for coin in staggering[10:]:
+        print(coin)
+        proposed_amounts[coin] = amounts[coin]
+
+    for coin, amount in amounts.items():
+        for i in range(amount):
+            if get_overlap() >= coin:
+                proposed_amounts[coin] += 1
+
 
 def get_overlap():
     """Calculates the overlap, that is still too much in the register for the proposed staggering."""
     proposed = 0
-    for i in range(0,13):
-        proposed += proposed_amounts[i] * staggering[i]
+    for coin in staggering:
+        proposed += proposed_amounts[coin] * coin
     return round(total - proposed - 100, 2)
 
+
 read_input(amounts)
-#dev_values(amounts)
 
 print("\nValues for table: " + str(amounts) + "\nCheck these again, to be sure you didn't make a typo.\n")
 
-for i in range(0,13):
-    total += amounts[i] * staggering[i]
+# Calculate coin amount value
+for coin in staggering:
+    total += amounts[coin] * coin
 
 total = round(total, 2)
-
 register_money = total - 100
-
 calculate_staggering()
 
 
 print("Sum: " + str(total))
 print("Rest in register: " + str(total - register_money)) # adjust later
-print("\nProposed staggering:\nAmount 100: " + str(proposed_amounts[0]) + "\nAmount  50: " + str(proposed_amounts[1]) + "\nAmount  20: " + str(proposed_amounts[2]) + "\nAmount  10: " + str(proposed_amounts[3]) + "\nAmount   5: " + str(proposed_amounts[4]) + "\nAmount   2: " + str(proposed_amounts[5]) + "\nAmount   1: " + str(proposed_amounts[6]) + "\nAmount  .5: " + str(proposed_amounts[7]) + "\nAmount  .2: " + str(proposed_amounts[8]) + "\nAmount  .1: " + str(proposed_amounts[9]) + "\nAmount .05: " + str(proposed_amounts[10]) + "\nAmount .02: " + str(proposed_amounts[11]) + "\nAmount .01: " + str(proposed_amounts[12]))
+print("\nProposed staggering for the envelope:")
+for coin, amount in proposed_amounts.items():
+    print("Amount {}: {}".format(coin, amount))
 
 now = datetime.datetime.now()
-print("\nInscription for envelope:\nName     " + name + "\nDate     " + str(now.day) + "." + str(now.month) + "." + str(now.year))
+print("\nInscription for envelope:\nName     " + name + "\nDate     " + now.strftime("%d.%m.%Y"))
 print("Money    " + str(round(total - 100, 2)))
